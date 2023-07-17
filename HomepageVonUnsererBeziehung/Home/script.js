204b01189349415902752e24f5cacda46ee8576e
Nikita88888 <74507770+Nikita88888@users.noreply.github.com>
2023-07-17 22:34:20 +0200

    Add files via upload

--- HomepageVonUnsererBeziehung/Home/script.js
+++ HomepageVonUnsererBeziehung/Home/script.js
@@ -5,16 +5,20 @@ dreiUndZwanzig_text = document.getElementById("dreiUndZwanzig_text");
 fotosBtn.onclick = function () {
     window.location.href = "../GemeinsameFotos/index.html";
 }
+
 dreiUndZwanzig_btn.onclick = function () {
     if (confirm("Dieser Text ist mit einer Sicherheitsfrage blockiert, um den Text anzuzeigen, muss die Frage beantwortet werden.")) {
         dreiUndZwanzig_password_input_value = prompt("Wann haben wir die eine Sache in Spanien zum ersten Mal gemacht?")
         if (dreiUndZwanzig_password_input_value == "") {
-            alert("Das Eingabefeld ist leer.")
+            alert("Das Eingabefeld ist Leer.")
         }
-        else if (dreiUndZwanzig_password_input_value == "14.07.2023") {
-            alert("Das Password ist richtig.")
+        else if (dreiUndZwanzig_password_input_value == "14.07.2023" || dreiUndZwanzig_password_input_value == "14072023") {
+            alert("Das Password ist Richtig.")
             dreiUndZwanzig_btn.style.display = "none";
             dreiUndZwanzig_text.style.display = "block";
+        }
+        else {
+            alert("Das Password ist Falsch.")
         }
     }
 }
@@ -26,4 +30,4 @@ document.addEventListener("visibilitychange", function() {
     } else if (document.visibilityState === "visible") {
         location.reload()
     }
-  });
+});
